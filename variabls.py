a8473c44a25dc07131c84d51c1b9cc67128eab1e
zaidalshreef <zaidalshreef@gmail.com>
2018-03-14 06:13:03 +0300

    new file

---
+++ variabls.py
@@ -0,0 +1,31 @@
+
+def  main():
+    #int and float
+    numflot=31/5
+    print(numflot)
+    numint=int(numflot)
+    print(numint)
+    numWithouCasting=(5*10)
+    print(numWithouCasting)
+    numToflot=float(5*10)
+    print(numToflot)
+    #strig
+    #asking for input from the user
+    amont=input("enter intger : ")
+
+    print(amont,type(amont))
+    #changig the type of the input from string into int
+    amont=int(amont)
+
+    print(amont, type(amont))
+    data="my name is \nzaid"
+    print(data,type(data))
+    data2 = "{} is high".format(amont)
+    print(data2,type(data2))
+    datalen = "is good"
+    print(len(datalen))
+    print(datalen[1])
+
+
+
+if __name__ == '__main__':main()
