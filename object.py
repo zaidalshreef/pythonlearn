9fa033201f6ac403d173d451c4b3fd8502fc7826
zaidalshreef <zaidalshreef@gmail.com>
2018-03-14 05:18:00 +0300

    new

--- object.py
+++ object.py
@@ -5,3 +5,11 @@ agg=55
 print(agg,type(agg))
 slry=30000.50
 print(slry,type(slry))
+print("{} {}".format(agg,name))
+
+name2 ="zaid hussain"
+print(name2)
+n =name2.split()
+print(n,type(n))
+print(n[0])
+print(n[1])
