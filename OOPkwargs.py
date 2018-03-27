5be33421413a9f85c7e14bc8d1ffda95cd03c54b
zaidalshreef <zaidalshreef@gmail.com>
2018-03-27 13:57:26 +0300

    new

--- OOPkwargs.py
+++ OOPkwargs.py
@@ -3,23 +3,18 @@ class car:
     def __init__(self,**kwargs):
         self.data = kwargs
     def get(self):
-        print(" owner {}".format((self.data["owner"])),end="")
-        print(" car model {}".format((self.data["model"])),end="")
-        print(" year {}".format((self.data["year"])))
+           print(self.data)
     def setmodel(self,model):
         self.data["model"] = model
     def getmodel(self):
-        print(" car model {}".format((self.data["model"])))
-
-
+        print(" model : {}".format((self.data["model"])))
 def  main():
-    mycar = car(owner="zaid",model="ki",year=2018)
+    mycar = car(owner="zaid",model="corvet",year=2018,price=2012220,speed=300)
     mycar.get()
-    farsCar = car(owner="fars",model="ki",year=2018)
+    farsCar = car(owner="fars",model="camry",year=2018)
     farsCar.get()
     mycar.setmodel("kia")
     mycar.getmodel()
     farsCar.setmodel("sany")
     farsCar.getmodel()
-
 if __name__ == '__main__':main()
